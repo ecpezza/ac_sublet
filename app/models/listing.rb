26aaefda4e11703e70d5be81f071786ae736193f
Elizabeth C. Pezza <ecpezza@users.noreply.github.com>
2017-05-23 10:23:12 -0500

    class
    first iteration

---
+++ app/models/listing.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: listings
+#
+#  id          :integer          not null, primary key
+#  bedrooms    :integer
+#  address     :string
+#  price       :float
+#  start_date  :datetime
+#  end_date    :datetime
+#  description :text
+#  user_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Listing < ApplicationRecord
+  validates :bedrooms, :address, :price, :start_date, :end_date, :description, :user_id, presence: true
+  has_many :photos, :dependent => :destroy
+  belongs_to :user
+end
---
+++ app/models/photo.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: photos
+#
+#  id         :integer          not null, primary key
+#  image_url  :string
+#  listing_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Photo < ApplicationRecord
+  belongs_to :listing
+
+  validates :image_url, :listing_id, presence: true
+end
